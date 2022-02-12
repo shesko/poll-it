7e3cd2153cfd4577387e1e39120370aa9d2f228d
Francesco Vanduynslager <francesco@getnude.com>
2022-02-12 16:31:32 +0100

    add poller with continuePolling function
    the client can pass in a continuePolling function which
    is evaluated every time a poll is executed. If the function
    returns true then the polling should return the last result
    and stop polling.

--- src/Poller.ts
+++ src/Poller.ts
@@ -1,5 +1,31 @@
-export default class Poller {
-    poll(): boolean {
-        return true;
+type PromiseResolve<T> = (result: T) => void
+type PromiseReject = (error: any) => void
+
+export default class Poller<T> {
+    private readonly fn: () => T;
+    private readonly continuePolling: (result?: T) => boolean = () => true;
+
+    constructor(config: PollerConfig<T>) {
+        this.fn = config.fn
+        this.continuePolling = config.continuePolling || this.continuePolling;
     }
+
+    async poll(): Promise<T> {
+        const execute = async (resolve: PromiseResolve<T>, reject: PromiseReject) => {
+            const result = this.fn();
+
+            if (this.continuePolling(result)) {
+                setTimeout(() => execute(resolve, reject));
+            } else {
+                resolve(result);
+            }
+        };
+
+        return new Promise(execute);
+    }
+}
+
+export interface PollerConfig<T> {
+    fn: () => T,
+    continuePolling: (result?: T) => boolean
 }
--- src/__tests__/Poller.test.ts
+++ src/__tests__/Poller.test.ts
@@ -1,8 +1,30 @@
 import Poller from '../Poller'
+import { v4 as uuidv4 } from 'uuid';
 
 describe('Poller', () => {
-    it('should return true', () => {
-        expect((new Poller()).poll()).toBe(true);
+    it('should execute the given function and return its result', async () => {
+        const output = uuidv4().toString();
+        const fn = jest.fn(() => output);
+        const poller = new Poller({ fn, continuePolling: () => false });
+
+        const result = await poller.poll();
+
+        expect(result).toEqual(output);
+    });
+
+    it('should continue polling until continuePolling resolved to false', async () => {
+        const output = uuidv4().toString();
+        let callsCount = 0
+        const continuePolling = () => {
+            callsCount++;
+            return callsCount < 3;
+        }
+        const fn = jest.fn(() => output);
+        const poller = new Poller({ fn, continuePolling });
+
+        const result = await poller.poll();
+
+        expect(result).toEqual(output);
+        expect(fn).toHaveBeenCalledTimes(3);
     });
 })
-
